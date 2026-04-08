995c07f27e8d172bd1e8acc8d2609607232597ee
workstation <workstation@PC.local>
2026-04-08 13:40:59 +0000

    Added append_slash=false

--- app/users/views.py
+++ app/users/views.py
@@ -3,7 +3,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Profiles
 from .serializers import ProfileSerializer # Make sure you have this in serializers.py
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
+@method_decorator(csrf_exempt, name='dispatch') # Add this decorator!
 class ProfileDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
@@ -11,6 +14,5 @@ class ProfileDetailView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
-    # Add this so your 'api.post' from React works
     def post(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
